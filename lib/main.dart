db22b9195d4451c4fad56e9ae4f4c85c50342845
hariz20 <hariz.izzuddin20@gmail.com>
2023-02-28 09:26:40 +0700

    28/02/2023

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Text(
-            "Hello World",
+            "Hello",
             style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
           ),
         ),
